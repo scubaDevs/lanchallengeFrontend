13444704edf88f1d6c7b56eb871f0aff6307098d
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2022-04-20 23:51:27 -0300

    new

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import '../styles/login.module.scss';
+
+const Login = ()=>{
+    return(
+        <div className='teste'>
+            <h1>Esta é a página de login!</h1>
+        </div>
+    )
+};
+
+export default Login;
